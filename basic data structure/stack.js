f95e80fcfa1f23550264694b14b538117a6985ee
ckw <tplwang@163.com>
2019-03-24 17:00:43 +0800

    add data stucture

---
+++ basic data structure/stack.js
@@ -0,0 +1,26 @@
+// Stack
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+
+    push(item) {
+        this.stack.push(item);
+    }
+
+    pop() {
+        this.stack.pop();
+    }
+
+    peek() {
+        return this.stack[this.getCount() - 1];
+    }
+
+    getCount() {
+        return this.stack.length;
+    }
+
+    isEmpty() {
+        return this.getCOunt() === 0;
+    }
+}
